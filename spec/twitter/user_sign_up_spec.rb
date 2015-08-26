43783e2ee18cabae5f7b5b778396a68764b959db
vjdhama <vjdhama26@gmail.com>
2015-08-26 16:13:36 +0530

    completed check for user availability

--- spec/twitter/user_sign_up_spec.rb
+++ spec/twitter/user_sign_up_spec.rb
@@ -3,11 +3,16 @@ require "spec_helper"
 module Twitter
   describe "UserSignup" do
     let(:connection) { DatabaseConnection.new("twitter_spec", "testing") }
-    let(:user_sign_up) { UserSignUp.new("vijay", "123", connection.conn) }
+    let(:user_sign_up_1) { UserSignUp.new("vijay", "123", connection.conn) }
+    let(:user_sign_up_2) { UserSignUp.new("vjdhama", "123", connection.conn) }
+
 
     it "will check if user already exits" do
-      expect(user_sign_up.username_available?).to eq(false)
+      expect(user_sign_up_1.username_available?).to eq(false)
     end
       
+    it "will check if user not already present" do
+      expect(user_sign_up_2.username_available?).to eq(true)
+    end
   end
 end
