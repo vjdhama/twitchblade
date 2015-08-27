30a81dca72486d3170cdec8c722582055afce632
vjdhama <vjdhama26@gmail.com>
2015-08-27 11:22:40 +0530

    refacctored a spec

--- spec/twitter/user_spec.rb
+++ spec/twitter/user_spec.rb
@@ -8,8 +8,7 @@ module Twitter
     end
 
     before(:each) do
-      @user_sign_up_1 = User.new("vijay", "123", @connection.conn) 
-      @user_sign_up_2 = User.new("aninda", "123", @connection.conn)
+      @user = User.new("vijay", "123", @connection.conn)
       @connection.conn.exec("insert into users (username, password) values ('vijay', '123')")
       @connection.conn.exec("insert into users (username, password) values ('vjdhama', 'qza')")
     end
@@ -20,13 +19,13 @@ module Twitter
 
     it "should insert username password tuple into users table if valid" do
       username = "vjdhama"
-      @user_sign_up_2.sign_up
+      @user.sign_up
       expect(@connection.conn.exec("select username from users where username='#{username}'").ntuples).to eq(1)
     end
 
     it "should not insert username password tuple into users if username already present" do
       username = "vijay"
-      @user_sign_up_1.sign_up
+      @user.sign_up
       expect(@connection.conn.exec("select username from users where username='#{username}'").ntuples).to eq(1)
     end
   end
