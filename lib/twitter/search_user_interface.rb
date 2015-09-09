1c8078bd4133f9b842d726e05b3b517085bfa02a
vjdhama <vjdhama26@gmail.com>
2015-09-09 11:01:31 +0530

    search takes input from stdin

--- lib/twitter/search_user_interface.rb
+++ lib/twitter/search_user_interface.rb
@@ -1,11 +1,24 @@
 module Twitter
   class SearchUserInterface
+
     def initialize
 
     end
 
     def get_username
-      "vijay"
+      $stdin.gets.strip
+    end
+
+    private
+
+    def render_header
+      $stdin.puts "\n\tSearch"
+      $stdin.puts
+    end
+
+    def render_requirement
+      puts "Enter a username for searching"
+      print "Username : "
     end
   end
 end
--- spec/twitter/search_user_interface_spec.rb
+++ spec/twitter/search_user_interface_spec.rb
@@ -5,7 +5,8 @@ module Twitter
     before(:each) { @interface = SearchUserInterface.new }
 
     it "should get input from STDIN" do
-      expect(@interface.get_username).to eq("vijay")
+      allow($stdin).to receive(:gets).and_return("vjdhama")
+      expect(@interface.get_username).to eq("vjdhama")
     end
   end
 end
